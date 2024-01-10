722e50c662c022de0c0084d72d29e67cda2794a5
Taweechai Yuenyang <taweechai.yuenyang@outlook.com>
2024-01-10 13:20:36 +0700

    update env file

--- webbase/settings.py
+++ webbase/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n919u-1x#hdb8aaf$qba6+9-+@mla*ofaz81#mc6z2q)-=18%&'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'ADSads123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG_MODE'))
