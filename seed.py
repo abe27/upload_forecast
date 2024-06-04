9713247772028b22c5d4322dde39622a858a1082
Taweechai Yuenyang <taweechai.yuenyang@outlook.com>
2024-06-04 09:32:10 +0700

    update seed data

--- seed.py
+++ seed.py
@@ -1,14 +1,16 @@
 import os
+from os.path import join, dirname
 import subprocess
 
 def main():
-    dir = os.listdir("seed/")
+    sDir = join(dirname(__file__), '')
+    dir = os.listdir(f"{sDir}/seed/")
     dir.sort()
     for dd in dir:
-        files = [f for f in os.listdir(f"seed/{dd}")]
+        files = [f for f in os.listdir(f"{sDir}/seed/{dd}")]
         files.sort()
         for f in files:
-            fname = f"seed/{dd}/{f}"
+            fname = f"{sDir}/seed/{dd}/{f}"
             strCmd = f"python manage.py loaddata {fname}"
             print(strCmd)
 
